4017df9d21e860a0a6f58d46e7dcaf4b8b5c2d47
rsilveira65 <rsilveiracc@gmail.com>
2019-03-14 20:06:21 -0300

    Adding token validator service.

---
+++ application/wp-content/plugins/concepta/src/APIFetcherModules/Service/TokenValidatorService.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rafael
+ * Date: 14/03/19
+ * Time: 19:38
+ */
+
+namespace Concepta\APIFetcherModules\Service;
+
+/**
+ * Class TokenValidatorService
+ *
+ * @package Concepta\APIFetcherModules\Service
+ * @author Rafael Silveira <me@rsilveira.dev>
+ */
+class TokenValidatorService
+{
+    /**
+     * @param $token
+     */
+    public function setToken($token)
+    {
+        update_option( 'concepta_token', $token);
+    }
+
+    /**
+     * @return mixed|void
+     */
+    public function getToken()
+    {
+        $token = get_option('concepta_token');
+
+        return $token['access_token'];
+    }
+
+    /**
+     * @return bool
+     */
+    public function isTokenValid()
+    {
+        $token = get_option('concepta_token');
+
+        if (!$token) {
+            return false;
+        }
+
+        //Checking if the token get expired
+        if ((new \DateTime('now'))->getTimestamp() > strtotime($token['.expires'])) {
+            return false;
+        }
+
+        return true;
+    }
+}
