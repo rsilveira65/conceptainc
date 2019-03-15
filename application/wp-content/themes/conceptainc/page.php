224302d3809d983471851677502a47e44e5c3c2e
rsilveira65 <rsilveiracc@gmail.com>
2019-03-14 23:49:16 -0300

    Adding package-lock file.

--- application/wp-content/themes/conceptainc/page.php
+++ application/wp-content/themes/conceptainc/page.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
     <?php if (have_posts()): while (have_posts()) : the_post(); ?>
-
+        <h1><?php the_title(); ?> <a href="https://github.com/rsilveira65/conceptainc"><img src="https://avatars0.githubusercontent.com/u/9919?s=280&v=4" height="50" width="50"></a></h1>
         <?php the_content(); ?>
 
         <?php endwhile; ?>
